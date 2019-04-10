eaaec44dabe97e4c93ceb869cfc1d1a9ff704dfb
Prouser123 <jcahill773@gmail.com>
2019-04-10 14:02:30 +0100

    gunicorn config file

---
+++ gunicorn.conf.py
@@ -0,0 +1,25 @@
+# run with 'gunicorn -c gunicorn.conf.py server:app'
+
+import os
+import multiprocessing
+
+import main
+
+loglevel = 'info'
+errorlog = "-"
+# uncomment to log every request / access
+# accesslog = "-"
+
+
+port = os.getenv("PORT") or 5000
+bind = '0.0.0.0:' + str(port)
+# workers = 1
+workers = multiprocessing.cpu_count() * 2 + 1
+
+timeout = 3 * 60  # 3 minutes
+keepalive = 24 * 60 * 60  # 1 day
+
+capture_output = True
+
+# connect to xbox live
+main.main()
--- wsgi.py
+++
@@ -1,6 +0,0 @@
-import main
-from server import app as application
-
-if __name__ == "__main__":
-    main.main()
-    application.run()
