278b66e445ea83974610753f26f6d541de4d6df3
James <jcahill773@gmail.com>
2021-06-09 17:48:50 +0100

    Add json cached route decorator

--- cached_route.py
+++ cached_route.py
@@ -1,3 +1,5 @@
+from flask import Response
+
 import functools
 import logging
 
@@ -21,11 +23,31 @@ class CachedRoute:
             # Return the function output (<Response>) wrapped in add_cache_headers
             return cache.add_cache_headers(value)
         return wrapper_decorator
+    
+    def __makeJsonResponse__(self, func):
+        @functools.wraps(func)
+        def wrapper_decorator(*args, **kwargs):
+            # Call the function
+            data = func(*args, **kwargs)
+            # Debug log the original return type
+            logger.debug("__makeJsonResponse__:func return '%s'" % str(data))
+            # Convert the text into a JSON response
+            res = Response(response=data, mimetype='application/json')
+            logger.debug("__makeJsonResponse__:created '%s'" % str(res))
+            # Return the response
+            return res
+        return wrapper_decorator
+
 
     def route(self, path, timeout=300):
         def dec(func):
             logger.debug("Registering route '%s' with cache timeout '%is'" % (path, timeout))
             return self.app.route(path)(cache.cache.cached(timeout)(self.__addHeaders__(func)))
+        return dec
+    
+    def jsonRoute(self, path, timeout=300):
+        def dec(func):
+            return self.route(path, timeout)(self.__makeJsonResponse__(func))
         return dec
     
 
