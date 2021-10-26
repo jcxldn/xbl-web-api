2e3a8ef2aa2b9530229ededd0a1c8427757e1a02
James <jcahill773@gmail.com>
2021-10-26 11:17:59 +0100

    Add logger and cache to blueprintprovider init

--- providers/BlueprintProvider.py
+++ providers/BlueprintProvider.py
@@ -2,20 +2,26 @@ from abc import ABC, abstractmethod
 from quart import Blueprint, jsonify
 
 from providers.XblDecoratorProvider import XblDecorator
+from providers import LoggingProvider
 
 class BlueprintProvider(ABC):
     # src: https://stackoverflow.com/a/11408458
     def _type(self):
         return self.__class__.__name__
 
-    def __init__(self, loop, xbl_client):
+    def __init__(self, loop, xbl_client, cache):
         self.app = Blueprint(self._type(), self._type())
-        print("Init blueprint with name '%s'" % self.app.name)
+        self.logger = LoggingProvider.getLogger(__name__)
+
+        self.logger.info("Init blueprint with name '%s'" % self.app.name)
 
         self.loop = loop
         self.xbl_client = xbl_client
 
-        self.xbl_decorator = XblDecorator(self.app, self.loop, None)
+        # Set cache variable
+        self.cache = cache
+        # Pass through cache variable to XblDecorator instance (for openXboxRoute caching in blueprints)
+        self.xbl_decorator = XblDecorator(self.app, self.loop, cache)
 
         # Shortcut to access openXboxRoute
         self.openXboxRoute = self.xbl_decorator.openXboxRoute
