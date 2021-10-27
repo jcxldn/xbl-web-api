02517a3c5896d2463a69501e6ee1b1238f78ed0a
James <jcahill773@gmail.com>
2021-10-27 16:22:30 +0100

    Send gamertag route though xuid route to hit cache

--- routes/presence.py
+++ routes/presence.py
@@ -5,16 +5,13 @@ from providers.BlueprintProvider import BlueprintProvider
 from providers.LoopbackRequestProvider import LoopbackRequestProvider
 
 class Presence(BlueprintProvider, LoopbackRequestProvider):
-    async def getSinglePresence(self, xuid):
-        # Currently getSinglePresence is not part of any release
-        #return await self.xbl_client.presence.get_presence(str(xuid))
-
-        return (await self.xbl_client.presence.get_presence_batch([str(xuid)]))[0]
-    
     def routes(self):
         @self.openXboxRoute("/xuid/<int:xuid>")
         async def xuid(xuid):
-            return await self.getSinglePresence(xuid)
+            # Currently getSinglePresence is not part of any release
+            #return await self.xbl_client.presence.get_presence(str(xuid))
+
+            return (await self.xbl_client.presence.get_presence_batch([str(xuid)]))[0]
 
         @self.openXboxRoute("/gamertag/<gamertag>")
         async def gamertag(gamertag):
@@ -25,14 +22,21 @@ class Presence(BlueprintProvider, LoopbackRequestProvider):
             
             # This is a callback for after the localhost request has been made.
             async def on_finish(res: ClientResponse):
-                # 1. Check status code
+                # Check status code
                 # (If we get here we already have a response object so no need to check for that)
                 if (res.status == 200):
                     xuid = str((await res.json())["xuid"])
                     print("Got XUID: %s" % xuid)
-                    # Now that we have the xuid (albeit with a bad method) we can call getSinglePresence as before
-                    # TODO: caching method for this as well?
-                    return await self.getSinglePresence(xuid)
+
+                    # Now we're gonna do the same thing but with /presence/xuid
+                    # This function serves as a callback after we look up the presence via XUID.
+                    async def on_presence_lookup_finish(presenceRes: ClientResponse):
+                        return jsonify(await presenceRes.json())
+                    
+                    # Get presence via xuid, using above function as a callback
+                    # As below, the callback will return a response so we can just return the output once awaited
+                    return await self.get("http://localhost:%i/presence/xuid/%s" % (self.xbl_client._xbl_web_api_current_port, xuid), on_presence_lookup_finish)
+
                 else:
                     # Passthough response content (handled in above route)
                     response = jsonify(await res.json())
