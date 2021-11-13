95e256fdfa6b524487d38e957f95a3912c2a45ee
James <jcahill773@gmail.com>
2021-11-13 11:11:18 +0000

    Use pagedResponseModifier for achievement routes

--- routes/achievements.py
+++ routes/achievements.py
@@ -1,48 +1,35 @@
-from quart import request
-
 from providers.BlueprintProvider import BlueprintProvider
-
-
-def tryParseInt(val, fallback):
-    try:
-        if val.isdigit():
-            return int(val)
-        else:
-            return fallback
-    except AttributeError:
-        # val is likely NoneType
-        return fallback
 
 class Achievements(BlueprintProvider):
     def routes(self):
         # ---------- Recent Achievements for user ----------
         @self.openXboxRoute("/1/recent/<int:xuid>")
-        async def recent1(xuid):
-            return await self.xbl_client.achievements.get_achievements_xboxone_recent_progress_and_info(xuid)
+        @self.pagedResponseModifier()
+        async def recent1(xuid, continuationToken):
+            return await self.xbl_client.achievements.get_achievements_xboxone_recent_progress_and_info(xuid, extra_params = { "continuationToken": continuationToken })
         
-        @self.openXboxRoute("/360/recent/<int:xuid>")
+        @self.openXboxRoute("/360/recent/<int:xuid>") # 500 when using continuationtoken
         async def recent360(xuid):
             return await self.xbl_client.achievements.get_achievements_xbox360_recent_progress_and_info(xuid)
 
         # ---------- Achievements for user & title ----------
-        # gay: continuationToken
         @self.openXboxRoute("/1/titleprogress/<int:xuid>/<int:titleid>")
-        async def titleprogress1(xuid, titleid):
-            continuationToken = tryParseInt(request.args.get("continuationToken"), 0)
-            self.logger.debug("Found continuationToken: '%s'" % str(continuationToken))
-
+        @self.pagedResponseModifier()
+        async def titleprogress1(xuid, titleid, continuationToken):
             return await self.xbl_client.achievements.get_achievements_xboxone_gameprogress(xuid, titleid, extra_params = {"continuationToken": continuationToken })
 
         # Note that this endpoint displays all achievements as locked.
         @self.openXboxRoute("/360/titleprogress/all/<int:xuid>/<int:titleid>")
-        async def titleprogress360all(xuid, titleid):
-            return await self.xbl_client.achivements.get_achivements_xbox360_all(xuid, titleid)
+        @self.pagedResponseModifier()
+        async def titleprogress360all(xuid, titleid, continuationToken):
+            return await self.xbl_client.achievements.get_achievements_xbox360_all(xuid, titleid, extra_params = {"continuationToken": continuationToken })
 
         @self.openXboxRoute("/360/titleprogress/earned/<int:xuid>/<int:titleid>")
-        async def titleprogress360earned(xuid, titleid):
-            return await self.xbl_client.achivements.get_achivements_xbox360_earned(xuid, titleid)
+        @self.pagedResponseModifier()
+        async def titleprogress360earned(xuid, titleid, continuationToken):
+            return await self.xbl_client.achievements.get_achievements_xbox360_earned(xuid, titleid, extra_params = {"continuationToken": continuationToken })
 
         # ---------- Achievement details for user / scid / achievementID ----------
-        @self.openXboxRoute("/1/titleprogress/detail/<int:xuid>/<scid>/<int:achievementid>")
+        @self.openXboxRoute("/1/titleprogress/detail/<int:xuid>/<scid>/<int:achievementid>") # continuationToken does nothing (not required anyways? - only one achievement returned)
         async def titleprogress1detail(xuid, scid, achievementid):
             return await self.xbl_client.achievements.get_achievements_detail_item(xuid, scid, achievementid)
