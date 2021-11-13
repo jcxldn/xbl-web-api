b68f57479deceaf4875fc9b5b37e0ecc1e79b044
James <jcahill773@gmail.com>
2021-11-13 15:09:05 +0000

    Initial img shortcut route for catalog

--- routes/catalog.py
+++ routes/catalog.py
@@ -1,10 +1,43 @@
+from aiohttp import ClientResponse
+from functools import partial
+from quart import redirect
 from providers.BlueprintProvider import BlueprintProvider
-
+from providers.LoopbackRequestProvider import LoopbackRequestProvider
 from xbox.webapi.api.provider.catalog.models import AlternateIdType, FieldsTemplate
 
-class Catalog(BlueprintProvider):
+class Catalog(BlueprintProvider, LoopbackRequestProvider):
     async def __get_product_from_alt_id(self, id, type: AlternateIdType, fields: FieldsTemplate):
         return await self.xbl_client.catalog.get_product_from_alternate_id(id, type, fields)
+
+    async def __boxart_callback_on_product_lookup_finish(self, imagePurpose: str, lookupRes: ClientResponse):
+        # Check status code
+        if (lookupRes.status == 200):
+            # Get JSON data
+            json = await lookupRes.json()
+
+            # Get image array
+            image_array = json["products"][0]["localizedProperties"][0]["images"]
+
+            # Filter the list for the object containing the boxart
+            dat = (list(filter(lambda i:i["imagePurpose"] == imagePurpose, image_array)))[0]
+            
+            # Get the  URL, replacing // with https:// for better compatibility
+            url = dat["uri"].replace("//", "https://")
+
+            # Create a 302 (Found) response
+            return redirect(url, code=302)
+
+    async def __boxart_make_loopback_request(self, image, lookupType, id):
+        def imageTypes(x):
+            return {
+                "boxart": "BoxArt",
+                "keyart": "BrandedKeyArt"
+            }[x]
+        
+        # convert image purpose into the exact key that the API uses
+        imagePurpose = imageTypes(image)
+        # Make the request, using functools.partial to pass the imagePurpose through to the callback
+        return await self.get("http://localhost:%i/catalog/lookup/%s/%s/browse" % (self.xbl_client._xbl_web_api_current_port, lookupType, id), partial(self.__boxart_callback_on_product_lookup_finish, imagePurpose))
 
     def routes(self):
         # ---------- Lookup via bigID (store ID) ----------
@@ -31,3 +64,22 @@ class Catalog(BlueprintProvider):
             # pfn: ProductFamilyName (shorthand often used my microsoft)
             fieldsEnum = FieldsTemplate.BROWSE if fields == "browse" else FieldsTemplate.DETAILS
             return await self.__get_product_from_alt_id(id, AlternateIdType.PACKAGE_FAMILY_NAME, fieldsEnum)
+        
+        # ---------- Shortcut routes to get various images (eg. boxart, keyart, etc) ----------
+        # Using loopback requests to above routes - See xuid.py#L34
+        # Callback defined above outside of routes()
+        @self.xbl_decorator.cachedRoute("/img/<any(boxart, keyart):image>/bigid/<id>")
+        async def boxart_bigid(image, id):
+            return await self.__boxart_make_loopback_request(image, "bigid", id)
+
+        @self.xbl_decorator.cachedRoute("/img/<any(boxart, keyart):image>/legacyproductid/<int:id>")
+        async def boxart_legacy(image, id):
+            return await self.__boxart_make_loopback_request(image, "legacyproductid", id)
+
+        @self.xbl_decorator.cachedRoute("/img/<any(boxart, keyart):image>/titleid/<int:id>")
+        async def boxart_titleid(image, id):
+            return await self.__boxart_make_loopback_request(image, "titleid", id)
+
+        @self.xbl_decorator.cachedRoute("/img/<any(boxart, keyart):image>/pfn/<id>")
+        async def boxart_pfn(image, id):
+            return await self.__boxart_make_loopback_request(image, "pfn", id)
